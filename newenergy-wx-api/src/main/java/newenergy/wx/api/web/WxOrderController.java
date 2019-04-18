978b60496771af55ad1bd4a5a17574c15801cac1
Caiminm1n <M201876091@hust.edu.cn>
2019-04-18 17:20:58 +0800

    添加自定义用户授权token的验证及其注解

--- newenergy-wx-api/src/main/java/newenergy/wx/api/web/WxOrderController.java
+++ newenergy-wx-api/src/main/java/newenergy/wx/api/web/WxOrderController.java
@@ -1,5 +1,6 @@
 package newenergy.wx.api.web;
 
+import newenergy.wx.annotation.OAuthUser;
 import newenergy.wx.api.service.WxOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 微信充值
+ * @author yangq
+ */
 @RestController
 @RequestMapping("/wx/order")
 public class WxOrderController {
     @Autowired
     private WxOrderService wxOrderService;
 
+    /**
+     * @author yangq
+     * 添加@OAuthUser注解
+     * @param openId
+     * @param body
+     * @param request
+     * @return
+     */
     @PostMapping("submit")
-    public Object submit(@RequestBody String body,HttpServletRequest request){
-        return wxOrderService.submit(body,request);
+    public Object submit(@OAuthUser String openId, @RequestBody String body, HttpServletRequest request){
+        return wxOrderService.submit(openId,body,request);
     }
 
     @PostMapping("pay-notify")
--- newenergy-wx-api/src/main/java/newenergy/wx/product/util/AdvancedUtil.java
+++ newenergy-wx-api/src/main/java/newenergy/wx/product/util/AdvancedUtil.java
@@ -7,6 +7,7 @@ import newenergy.wx.product.pojo.WeixinOauth2Token;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
