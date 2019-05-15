5f4d6d76f9f160715643c73a9e04325f94860ecd
SimpleFate <714676641@qq.com>
2019-05-15 18:58:36 +0800

    配置多线程的定时任务

--- newenergy-wx-api/src/main/java/newenergy/wx/api/controller/BindingController.java
+++ newenergy-wx-api/src/main/java/newenergy/wx/api/controller/BindingController.java
@@ -13,6 +13,8 @@ import newenergy.wx.api.service.MsgService;
 import newenergy.wx.api.util.WxXmlUtil;
 import newenergy.wx.product.manager.UserTokenManager;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +33,8 @@ import java.util.*;
 @RestController
 @RequestMapping("wx")
 public class BindingController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private MsgService msgService;
     @Autowired
@@ -102,6 +106,7 @@ public class BindingController {
             return msgService.buildText(text.getFromUserName(),text.getToUserName(),greeting);
         }else if("text".equals(text.getMsgType())){
             String content = text.getContent();
+            log.info("receive message from wx : " + content);
             if(!binding.equals(content) && !unbinding.equals(content) && !searching.equals(content)) return "";
             String param = String.format("?token=%s&state=%d",UserTokenManager.generateTokenWithOpenId(text.getFromUserName()).getToken(),
                     msgService.getBindState(text.getFromUserName()));
