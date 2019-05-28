7d35b34884f47f9f8953d2d5d889010fea65661d
SimpleFate <714676641@qq.com>
2019-05-28 14:16:36 +0800

    备份还原

--- newenergy-db/src/main/java/newenergy/db/service/BackupService.java
+++ newenergy-db/src/main/java/newenergy/db/service/BackupService.java
@@ -13,44 +13,31 @@ import java.io.*;
  */
 @Service
 public class BackupService {
-//    @Value("${backup.basedir}")
-//    String path;
+    @Value("${backup.basedir}")
+    String path;
     @Value("${backup.username}")
     String username;
     @Value("${backup.password}")
     String password;
     @Value("${backup.database}")
     String database;
-//    @Value("${backup.loaddir}")
-//    String loadPath;
+
     @Value("${backup.loadname}")
     String loadName;
     String cmdWin = "cmd /c ";
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
     public void saveBackup() throws Exception{
-        String filePath = "backup" + TimeUtil.getDateString(TimeUtil.getUTCNow()) + ".sql";
+        String filePath = path + "backup" + TimeUtil.getDateString(TimeUtil.getUTCNow()) + ".sql";
         String cmd = String.format("mysqldump -u%s -p%s %s -r %s --skip-lock-tables",
                 username,password,database,filePath);
         //服务器上
+        cmd = "C:\\mysql-8.0.16-winx64\\bin\\".concat(cmd);
         cmd = cmdWin.concat(cmd);
 
         logger.info(cmd);
         Process process = Runtime.getRuntime().exec(cmd);
 
-        try(InputStream is1 = process.getErrorStream();
-            InputStream is2 = process.getInputStream();
-            BufferedReader br1 = new BufferedReader(new InputStreamReader(is1));
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(is2))){
-
-            StringBuilder sb1 = new StringBuilder();
-            br1.lines().forEach(sb1::append);
-            logger.info("备份错误：" + sb1);
-
-            StringBuilder sb2 = new StringBuilder();
-            br2.lines().forEach(sb2::append);
-            logger.info("备份输出："+sb2);
-        }
 
         int processComplete = process.waitFor();
         if (processComplete == 0) {
@@ -61,17 +48,17 @@ public class BackupService {
         }
     }
     public void loadBackup() throws Exception{
-        String targetFile = loadName;
+        String targetFile = path+loadName;
 //        String[] execCMD = new String[]{"mysql", database, "-u" + username, "-p" + password, "-h localhost", "-P 3306", "<", targetFile};
         String cmd = String.format("mysql %s -u%s -p%s -h localhost -P 3306",database,username,password);
 
         //服务器上
+        cmd = "C:\\mysql-8.0.16-winx64\\bin\\".concat(cmd);
         cmd = cmdWin.concat(cmd);
 
         logger.info(cmd);
         Process process = Runtime.getRuntime().exec(cmd);
         OutputStream os = process.getOutputStream();
-        InputStream is = process.getErrorStream();
 
 
         FileInputStream fis = new FileInputStream(targetFile);
@@ -82,21 +69,6 @@ public class BackupService {
         }
         fis.close();
         os.close();
-
-        try(InputStream is1 = process.getErrorStream();
-            InputStream is2 = process.getInputStream();
-            BufferedReader br1 = new BufferedReader(new InputStreamReader(is1));
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(is2))){
-
-            StringBuilder sb1 = new StringBuilder();
-            br1.lines().forEach(sb1::append);
-            logger.info("还原错误：" + sb1);
-
-            StringBuilder sb2 = new StringBuilder();
-            br2.lines().forEach(sb2::append);
-            logger.info("还原输出："+sb2);
-        }
-
 
 
         int processComplete = process.waitFor();
